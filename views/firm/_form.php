5f178f4ff55bfb697d86f8fed1b74200c27f77b9
Bp3g3H <zaimpuls1@gmai.com>
2018-07-22 17:27:50 +0300

    1st push

--- views/firm/_form.php
+++ views/firm/_form.php
@@ -25,6 +25,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'country')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'owner_name')->textInput(['maxlength' => true]) ?>
+<!--    test-->
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
